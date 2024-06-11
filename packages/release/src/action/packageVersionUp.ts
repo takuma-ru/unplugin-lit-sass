30637ed63e47d34029505da742218df4493768c0
takuma-ru <kanekotakuma@team-lab.com>
2024-06-11 12:40:23 +0900

    ✨ cmd options

--- packages/release/src/action/packageVersionUp.ts
+++ packages/release/src/action/packageVersionUp.ts
@@ -1,8 +1,8 @@
-import { execSync } from "node:child_process";
 import { readFileSync, writeFileSync } from "node:fs";
 import { join } from "node:path";
 import consola from "consola";
 import { type ReleaseType, inc } from "semver";
+import { cmd } from "../utils/cmd";
 import type { ReleaseSchemaType } from "../validation/validation";
 
 export const packageVersionUp = ({
@@ -13,9 +13,19 @@ export const packageVersionUp = ({
   const JoinedPackageJsonPath = join(__dirname, packageJsonPath);
   let packageJson = JSON.parse(readFileSync(JoinedPackageJsonPath, "utf8"));
 
-  const currentVersion = execSync(`npm show ${packageJson.name} version`, {
-    encoding: "utf8",
-  }).trim();
+  const currentVersion = cmd(`npm show ${packageJson.name} version`, {
+    execOptions: {
+      stdio: "pipe",
+      encoding: "utf8",
+    },
+    successCallback: (stdout) => {
+      return stdout.trim();
+    },
+    errorCallback: (error) => {
+      console.error(error.message);
+      return "0.0.0";
+    },
+  });
 
   consola.info(`Current version: ${currentVersion}`);
 
--- packages/release/src/utils/cmd.ts
+++ packages/release/src/utils/cmd.ts
@@ -1,11 +1,39 @@
-import { execSync } from "node:child_process";
+import {
+  type ExecSyncOptionsWithStringEncoding,
+  execSync,
+} from "node:child_process";
+import { consola } from "consola";
 
-export const cmd = (command: string) => {
+type Cmd = (
+  command: string,
+  options?: {
+    execOptions?: ExecSyncOptionsWithStringEncoding;
+    successCallback?: (stdout: string) => string;
+    errorCallback?: (error: NodeJS.ErrnoException) => string;
+  }
+) => string;
+
+export const cmd: Cmd = (command, options) => {
+  const { execOptions, successCallback, errorCallback } = options ?? {};
+  const { stdio = "inherit", encoding = "utf8" } = execOptions ?? {};
+
   try {
-    return execSync(command, { stdio: "inherit", encoding: "utf8" });
+    const stdout = execSync(command, { stdio, encoding, ...execOptions });
+
+    if (successCallback) {
+      return successCallback(stdout);
+    }
+
+    return stdout;
   } catch (error) {
-    console.error(`Error executing command: ${command}`);
-    console.error(error);
+    const err = error as NodeJS.ErrnoException;
+
+    consola.error(err.code ?? "", err.message);
+
+    if (errorCallback) {
+      return errorCallback(err);
+    }
+
     process.exit(1);
   }
 };
